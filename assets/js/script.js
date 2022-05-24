06653f9e0be15d7ee1eaf31546c0c268ec1dcbc7
Lunirs <dhong0925@gmail.com>
2022-05-24 00:22:56 -0400

    Added PseudoCode and set up initial html with header and hrefs to libraries

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var apiKey = "a615206688fa7fa4d02f98f637bd877b";
+var cityHist = [];
+var url = "https://api.openweathermap.org/data/2.5/weather";
+var date = moment().format("MM/DD/YYYY");
+
+// Dependencies
+// need search bar form element
+// need submit button element
+// need 5 day forecast container element so i can append new results to it
+// need user input element
+// need button elements for newly created button elements for history
+// need element for clear history button
+
+// Data States
+// need latitude and longitude
+// api link https://api.openweathermap.org/data/2.5/weather?lat=%7Blat%7D&lon=%7Blon%7D&appid=%7BAPI key}
+// link for cities "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+// need api key a615206688fa7fa4d02f98f637bd877b
+
+// Functions
+
+// need functions to get all the necessary data from the api
+// function to get user input's city weather
+// above should get me all of the necessary information from the api
+// temp
+// humidity
+// uv index
+// wind speed
+
+// User Interactions
+// submit button user interaction
+// newly created button from search history button interactions
